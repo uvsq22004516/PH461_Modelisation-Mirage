c2d3debe234ae3cb21a308c1a2f14bbf92b59cee
Alberic BEAUSSANT <alberic.beaussantpens.uvsq.fr>
2022-05-06 06:01:43 +0200

    Formules + calcul coordonnées

--- Fonctions.py
+++ Fonctions.py
@@ -1,51 +1,41 @@
-
 import math as m
 import numpy as np
 
-#Définition de variables pour test
+#tous les prints mis dans le code servent juste à tester le code et voir le retour
 
-alpha = 30
-alpha = np.radians(alpha)
-
-n1 = 1.1
-n2 = 1.01
-
-beta = 0
-
-#Nombre de couches et hauteur
-
-hauteur_tot = 600
-nb_couche = 3
+hauteur_tot = 1000 #hauteur du canvas en pixels
+n = 10 #nombre de couches (marche avec 13 max, au delà le module math bug, je ne sais pas pourquoi)
+n_init = 1.02 #indice de la 1ère couche
+n2 = n_init #initialisation
+grad = 0.1 #gradient d'indice
+hauteur = hauteur_tot/n
+beta = 0 #initialisation
+positions = [] #initialisation
 
 
-def calcul_angle_refl(): 
-
-    beta = m.asin((n1*m.sin(alpha))/n2)
-    return beta
-    return alpha
-
-#Coordonnées du point de départ
-
-pos_depart = [100, 100]
-pos_fin = [0,0]
-hauteur_couche = hauteur_tot/nb_couche
-coords_tot = []
-
-
-def position_point():
-    alpha = 31
-    for i in range(nb_couche):
-        a = hauteur_couche/m.tan(alpha)
-        beta = m.asin((n1*m.sin(alpha))/n2)
+def calcul_position():
+    x = 0
+    y = 0
+    alpha = 21
+    alpha = np.radians(alpha)
+    theta = m.asin(n_init/(n_init+grad))
+    for i in range(n):
+        n2 = n_init + grad*i
+        print(n2, "indice")
+        print(theta, "theta")
+        positions.append([])
+        positions[i].append(x)
+        positions[i].append(y)
+        x += hauteur//m.tan(alpha)        
+        beta = m.asin(n_init*m.sin(alpha)/n2)
         alpha = beta
-        pos_fin[0] = pos_depart[0] + a
-        pos_fin[1] = pos_depart[1] + hauteur_couche*(i+1)
-        coords_tot.append(pos_fin[0])
-        coords_tot.append(pos_fin[1])
-        coords_tot.append('')
+        if alpha <= theta:
+            y += hauteur
+        else:
+            y -= hauteur
+        print(alpha, "alpha")
+        print("")
 
-    print(coords_tot)
+    print(positions)
 
-
-calcul_angle_refl()
-position_point()
+calcul_position()
